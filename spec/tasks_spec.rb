33c6c5715fbe4f4af4ad87461581b3204f32cc0b
Ariel Salomon <asalomon@mylookout.com>
2012-05-08 14:46:02 -0700

    Add sharding support, for use in Spengler
     - extend with Rake:DSL to use sh method
    
    JIRA: SEC-671

--- spec/tasks_spec.rb
+++ spec/tasks_spec.rb
@@ -15,6 +15,11 @@ describe Opensesame do
       YAML.should_receive(:load_file).with(Rails.root + '/config/database.yml').and_return({})
       Opensesame.load_yml
     end
+
+    it 'should load the shards.yml file on Rails.root/config' do
+      YAML.should_receive(:load_file).with(Rails.root + '/config/shards.yml').and_return({})
+      Opensesame.load_yml('shards.yml')
+    end
   end
 
   describe '#config' do
@@ -37,6 +42,26 @@ describe Opensesame do
     end
   end
 
+  describe '#shard_config' do
+    let(:config) do
+      { 'database' => 'test_db',
+        'username' => 'test_user',
+        'password' => 'test_password'}
+    end
+
+    context 'with development configuration' do
+      before :each do
+        Opensesame.should_receive(:load_yml).and_return({'octopus' => {'development' => {'shards' => config}}})
+        Rails.stub(:env).and_return('development')
+      end
+
+      it 'should return the proper configuration for this environment' do
+        result = Opensesame.shard_config
+        result.should == config
+      end
+    end
+  end
+
   describe '#grants' do
     before :each do
       Opensesame.should_receive(:config).and_return(config)
@@ -54,6 +79,31 @@ describe Opensesame do
         it 'should have networks' do
           result = Opensesame.grants
           result.should include("GRANT ALL PRIVILEGES ON test_db.* TO 'test_user'@'127.0.0.0/24' IDENTIFIED BY 'test_password';")
+        end
+      end
+    end
+
+    context 'on shards' do
+      describe 'with multiple shards' do
+        let(:config) do
+          {
+            'shard1' => {
+              'database' => 'test_db1',
+              'username' => 'test_user',
+              'password' => 'test_password'
+            },
+            'shard2' => {
+              'database' => 'test_db2',
+              'username' => 'test_user',
+              'password' => 'test_password'
+            }
+          }
+        end
+
+        it 'should have both shards' do
+          result = Opensesame.grants
+          result.should include("GRANT ALL PRIVILEGES ON test_db1.* TO 'test_user'@'%' IDENTIFIED BY 'test_password';")
+          result.should include("GRANT ALL PRIVILEGES ON test_db2.* TO 'test_user'@'%' IDENTIFIED BY 'test_password';")
         end
       end
     end
